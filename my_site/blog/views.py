8738cfda2030179edc04dd599ea0d6ca00f7dd95
grombor <gromgaming@yahoo.com>
2022-03-22 22:33:16 +0100

    added dynamic content of the pages

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,12 +1,68 @@
 from django.shortcuts import render
+from datetime import date
+
+all_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "kamil",
+        "date": date(2022, 6, 12),
+        "title": "Mountain Hiking",
+        "exerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi consequatur similique.",
+        "content": """
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi consequatur similique, corporis totam repudiandae culpa quod tenetur laborum voluptatem autem dicta cum, quaerat aut sequi dolore cupiditate fugiat natus! Tempora!
+
+                    Eligendi consequatur similique, corporis totam repudiandae culpa quod tenetur laborum voluptatem autem dicta cum, quaerat aut sequi dolore cupiditate fugiat natus! Tempora!
+                """
+    },
+    {
+        "slug": "Bike-in-the-bare-mountains",
+        "image": "mountains.jpg",
+        "author": "kamil",
+        "date": date(2020, 3, 1),
+        "title": "Mountain Biking",
+        "exerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi consequatur similique.",
+        "content": """
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi consequatur similique, corporis totam repudiandae culpa quod tenetur laborum voluptatem autem dicta cum, quaerat aut sequi dolore cupiditate fugiat natus! Tempora!
+
+                    Eligendi consequatur similique, corporis totam repudiandae culpa quod tenetur laborum voluptatem autem dicta cum, quaerat aut sequi dolore cupiditate fugiat natus! Tempora!
+                """
+    },
+    {
+        "slug": "Programming-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "kamil",
+        "date": date(2021, 10, 11),
+        "title": "Mountain Programing",
+        "exerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi consequatur similique.",
+        "content": """
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi consequatur similique, corporis totam repudiandae culpa quod tenetur laborum voluptatem autem dicta cum, quaerat aut sequi dolore cupiditate fugiat natus! Tempora!
+
+                    Eligendi consequatur similique, corporis totam repudiandae culpa quod tenetur laborum voluptatem autem dicta cum, quaerat aut sequi dolore cupiditate fugiat natus! Tempora!
+                """
+    }
+
+]
+
+def get_date(post):
+    return post['date']
 
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post
+    })
