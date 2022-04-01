df9ddd906c9f81e8a9da872413d66759b6bb6722
grombor <gromgaming@yahoo.com>
2022-04-01 23:21:22 +0200

    added relations to db

--- books/book_outlet/models.py
+++ books/book_outlet/models.py
@@ -5,6 +5,45 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+    
+
+    class Meta:
+        verbose_name_plural = "Country"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+    
+    def full_name(self):
+        return f"{self.street} {self.postal_code} {self.city}"
+
+    def __str__(self):
+        return self.full_name()
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
 class Book(models.Model):
     title = models.CharField(
         max_length=50
@@ -14,9 +53,11 @@ class Book(models.Model):
             MinValueValidator(1), 
             MaxValueValidator(5)
             ])
-    author = models.CharField(
+    author = models.ForeignKey(
+        Author, 
+        on_delete=models.CASCADE, 
         null=True, 
-        max_length=100
+        related_name="books"
         )
     is_bestseller = models.BooleanField(
         default=False
@@ -27,12 +68,11 @@ class Book(models.Model):
         null=False,
         db_index=True
         )
-
+    published_countries = models.ManyToManyField(Country, )
 
     def get_absolute_url(self):
         return reverse("book-details", args=[self.slug])
     
-
     def __str__(self):
         return f"{self.title} ({self.rating})"
     
