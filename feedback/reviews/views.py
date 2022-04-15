4a788de8fe1a9f502bf4dd85239b9860910bf3ee
grombor <gromgaming@yahoo.com>
2022-04-15 21:30:24 +0200

    added more models and class models

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,33 +1,35 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
-from .models import Review
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from .models import Review
 
 # Create your views here.
 
-class ReviewView(View):
-    
-    def get(self, request):
-        
-        form = ReviewForm()
-
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
 
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['message'] = "Does it work?"
+        return context
+
+class ReviewListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
 
 
-def than_you(request):
-    return render(request, "reviews/thank_you.html")
+class SingleReviewView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
