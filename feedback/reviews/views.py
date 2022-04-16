fd36c98c761a5d23a70fdca8a4f6d84ec3312915
grombor <gromgaming@yahoo.com>
2022-04-16 22:18:16 +0200

    fixed session data reading

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,7 +1,6 @@
 from urllib import request
 from reviews.models import Review
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
@@ -34,14 +33,24 @@ class ReviewsListView(ListView):
     model = Review
     context_object_name = "reviews"
 
+
+
     
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get("favorite_review")
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+        return context
+
 
 class AddFavoriteView(View):
-    def post (self, request):
+    def post(self, request):
         review_id = request.POST["review_id"]
         request.session["favorite_review"] = review_id
         return HttpResponseRedirect("/reviews/" + review_id)
